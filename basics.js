b58791d050e8e5a49083aceca5c7941afe510802
amir830047agh@gmail.com <amir830047agh@gmail.com>
2025-08-14 19:25:02 +0330

    full course js day 1

---
+++ basics.js
@@ -0,0 +1,14 @@
+// supersimple.dev/projects/amazon 
+// create pop up
+alert("hello");
+// everything on the page
+document.body.innerHTML = "hello";
+// for rounding the number
+Math.round(2.8)
+// next line \n
+"hello\nthis is\namirmohammad"
+// type coercion
+'thing1' + 2 + 4 + 'dffbdbdb'
+// type of 
+typeof 'hello'
+// 
