62c2be7c78fd5e08d6f2b69776c9ac114d430b5f
amir830047agh@gmail.com <amir830047agh@gmail.com>
2025-08-24 22:38:21 +0330

    js oject full

--- basics.js
+++ basics.js
@@ -1,53 +1,50 @@
-// supersimple.dev/projects/amazon 
-// create pop up
-alert("hello");
-// everything on the page
-document.body.innerHTML = "hello";
-// for rounding the number
-Math.round(2.8)
-// next line \n
-"hello\nthis is\namirmohammad"
-// type coercion
-'thing1' + 2 + 4 + 'dffbdbdb'
-// type of 
-typeof 'hello'
-// 
-// or
-(|| && && )
-// falsy value 
-0 null undefined Nan ''
-NaN = 'text' * 4
-// short cut for if statement
-const result = true ? 'efsdgrw' : console.log('sdgsger');
+// // supersimple.dev/projects/amazon
+// // create pop up
+// alert("hello");
+// // everything on the page
+// document.body.innerHTML = "hello";
+// // for rounding the number
+// Math.round(2.8)
+// // next line \n
+// "hello\nthis is\namirmohammad"
+// // type coercion
+// 'thing1' + 2 + 4 + 'dffbdbdb'
+// // type of
+// typeof 'hello'
+// //
+// // or
+// (|| && && )
+// // falsy value
+// 0 null undefined Nan ''
+// NaN = 'text' * 4
+// // short cut for if statement
+// const result = true ? 'efsdgrw' : console.log('sdgsger');
 
-const message = false && 'hello';
-console.log(message);
+// const message = false && 'hello';
+// console.log(message);
 
-const currency = 'EUR' || 'USD';
+// const currency = 'EUR' || 'USD';
 
+// // default value for mul
+// function calc(prameter1, mul = 4){
+//   console.log(prameter1 * mul);
 
-// default value for mul
-function calc(prameter1, mul = 4){
-  console.log(prameter1 * mul);
-  
-}
+// }
 
-calc(2000 , 3)
-calc(2244) 
+// calc(2000 , 3)
+// calc(2244)
 
+// const pro = {
+//   name: "svsfdv",
+//   price: 13245
+// }
 
+// console.log(pro.name);
 
-const pro = {
-  name: "svsfdv",
-  price: 13245
-}
+// // delete one property
+// delete pro.newPropperty;
+// console.log(pro);
 
-console.log(pro.name);
-
-// delete one property
-delete pro.newPropperty;
-console.log(pro);
-
-
-
-
+console.log("hello".length);
+console.log("hello".toUpperCase());
+console.log("hello".toLowerCase());
